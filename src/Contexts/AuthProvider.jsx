d5a519767ae7960fa399023fa3608884caadf8df
prothush <prothushmashreky@gmail.com>
2025-06-09 12:37:54 +0600

    Successfully added update profile functionality while register user

--- src/Contexts/AuthProvider.jsx
+++ src/Contexts/AuthProvider.jsx
@@ -1,5 +1,5 @@
-import { createUserWithEmailAndPassword } from 'firebase/auth';
-import React, { createContext } from 'react';
+import { createUserWithEmailAndPassword, onAuthStateChanged, updateProfile } from 'firebase/auth';
+import React, { createContext, useEffect, useState } from 'react';
 import { auth } from '../firebase/firebase.init';
 
 
@@ -7,17 +7,39 @@ export const AuthContext= createContext()
 
 const AuthProvider = ({children}) => {
 
+    const [user, setUser]= useState("")
+    const [loading, setLoading]= useState(true)
+
     
 
     const createUser= (email, password)=>{
         return createUserWithEmailAndPassword(auth, email, password)
     }
 
+    const updateUser= (updatedData)=>{
+        return updateProfile(auth.currentUser, updatedData)
+    }
+
+
+
+    useEffect(()=>{
+       const unsubscribe= onAuthStateChanged(auth, ((currentUser)=>{
+            setUser(currentUser)
+            setLoading(false)
+        }))
+        return ()=>{
+            unsubscribe()
+        }
+    },[])
+
 
 
 
     const userInfo={
+        user,
+        setUser,
         createUser,
+        updateUser,
     }
 
 
--- src/Pages/Register.jsx
+++ src/Pages/Register.jsx
@@ -4,7 +4,7 @@ import { AuthContext } from '../Contexts/AuthProvider';
 
 const Register = () => {
 
-    const {createUser}= use(AuthContext)
+    const {createUser, updateUser, setUser}= use(AuthContext)
 
 
     const handleRegister= (e)=>{
@@ -18,6 +18,17 @@ const Register = () => {
         createUser(email, password)
         .then(result=>{
             console.log(result)
+            const user= result.user
+            updateUser({
+                displayName: name,
+                photoURL: photoUrl
+            })
+            .then(()=>{
+                setUser({...user, displayName: name, photoURL: photoUrl})
+            })
+            .catch(error=>{
+                setUser(user)
+            })
         })
         .catch(error=>{
             console.log(error)
@@ -33,13 +44,13 @@ const Register = () => {
                     <h1 className="text-3xl font-bold">Register now!</h1>
                     <form onSubmit={handleRegister} className="fieldset">
                         <label className="label">Name</label>
-                        <input type="text" name="name" className="input" placeholder="Your Name" />
+                        <input type="text" name="name" className="input" placeholder="Your Name" required/>
                         <label className="label">Email</label>
-                        <input type="email" name="email" className="input" placeholder="Your Email" />
+                        <input type="email" name="email" className="input" placeholder="Your Email" required/>
                         <label className="label">Photo URL</label>
-                        <input type="text" name="photoUrl" className="input" placeholder="Your Photo URL" />
+                        <input type="text" name="photoUrl" className="input" placeholder="Your Photo URL" required/>
                         <label className="label">Password</label>
-                        <input type="password" name="password" className="input" placeholder="Your Password" />
+                        <input type="password" name="password" className="input" placeholder="Your Password" required/>
                         <span className='text-red-500 text-xs'>Error</span>
                         <button type='submit' className="btn btn-neutral mt-4">Register</button>
                     </form>
