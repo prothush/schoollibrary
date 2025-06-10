c10b051f5c58cd378b8f5246bf104c25be878381
prothush <prothushmashreky@gmail.com>
2025-06-10 23:49:59 +0600

    Successfully added add book functionality

--- src/Pages/AddBook.jsx
+++ src/Pages/AddBook.jsx
@@ -1,29 +1,61 @@
-import React from 'react';
+import axios from 'axios';
+import React, { use } from 'react';
+import { AuthContext } from '../Contexts/AuthProvider';
 
 const AddBook = () => {
+
+    const { successMsg, errorMsg } = use(AuthContext)
+
+    const handleAddBook = (e) => {
+        e.preventDefault()
+        const form = e.target
+        const formData = new FormData(form)
+        const newBook = Object.fromEntries(formData.entries())
+        console.log(newBook)
+
+        axios.post("http://localhost:3000/books", newBook)
+            .then(res => {
+                if (res.data.insertedId) {
+                    successMsg("Books added successfully")
+                    form.reset()
+                }
+            })
+            .catch(error => {
+                
+                errorMsg(error)
+            })
+    }
+
+
+
     return (
         <div>
             <div className="max-w-3xl mx-auto p-6 mt-10 bg-base-100 shadow-md rounded-lg">
                 <h2 className="text-xl md:text-3xl font-bold text-center mb-8">Add Books</h2>
-                <form className="space-y-6">
+                <form onSubmit={handleAddBook} className="space-y-6">
                     <div>
                         <label className="label font-semibold">Image</label>
-                        <input type="text" name="image" placeholder="Photo URL" className="input input-bordered w-full" />
+                        <input type="text" name="image" placeholder="Photo URL" className="input input-bordered w-full" required/>
+                    </div>
+
+                    <div>
+                        <label className="label font-semibold">Title of Book</label>
+                        <input type="text" name="title" placeholder="Book title" className="input input-bordered w-full" required/>
                     </div>
 
                     <div>
                         <label className="label font-semibold">Quantity</label>
-                        <input type="number" name="quantity" placeholder="Quantity" className="input input-bordered w-full" />
+                        <input type="number" name="quantity" placeholder="Quantity" className="input input-bordered w-full" required/>
                     </div>
 
                     <div>
                         <label className="label font-semibold">Author Name</label>
-                        <input type="text" name="author" placeholder="Author Name" className="input input-bordered w-full" />
+                        <input type="text" name="author" placeholder="Author Name" className="input input-bordered w-full" required/>
                     </div>
 
                     <div>
                         <label className="label font-semibold">Category</label>
-                        <select className="select select-bordered w-full" name="category" defaultValue="">
+                        <select className="select select-bordered w-full" name="category" defaultValue="" required>
                             <option disabled value="">Select a category</option>
                             <option>Novel</option>
                             <option>Thriller</option>
@@ -35,12 +67,12 @@ const AddBook = () => {
 
                     <div>
                         <label className="label font-semibold">Short Description</label>
-                        <textarea name="description" className="textarea textarea-bordered w-full" placeholder="A brief description of the book"></textarea>
+                        <textarea name="description" className="textarea textarea-bordered w-full" placeholder="A brief description of the book" required></textarea>
                     </div>
 
                     <div>
                         <label className="label font-semibold">Rating</label>
-                        <input name="rating" type="number" className="input input-bordered w-full" />
+                        <input name="rating" type="number" className="input input-bordered w-full" required/>
                     </div>
                     <div className="pt-4">
                         <button type="submit" className="btn btn-primary w-full">Add Book</button>
