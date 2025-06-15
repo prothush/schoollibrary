98a35de451ab4d4d349c828fba77115e12030a9a
prothush <prothushmashreky@gmail.com>
2025-06-15 19:00:01 +0600

    Successfully added show available books functionality which quantity is greater than zero

--- src/Pages/AllBooks.jsx
+++ src/Pages/AllBooks.jsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from 'react';
 import AllBookCard from '../Components/AllBookCard';
 import { useLoaderData } from 'react-router';
 
@@ -8,15 +8,25 @@ const AllBooks = () => {
     
     const books= useLoaderData()
 
+    const [showAvailable, setShowAvailable]= useState(false)
+
+    const filterBooks= showAvailable ? books.filter(book=> book.quantity > 0) : books
+
+    const handleToggleBook = ()=>{
+        setShowAvailable(!showAvailable)
+    }
+
+
 
     
 
     return (
         <div className="w-11/12 mx-auto px-4 py-6">
             <h1 className="text-xl md:text-3xl font-bold mb-6 text-center">All Books</h1>
+            <button onClick={handleToggleBook} className='btn btn-primary mb-3'>Show {showAvailable ? "All" : "Available"} Books</button>
 
             <div className="grid gap-6 sm:grid-cols-1 md:grid-cols-2 lg:grid-cols-3">
-                {books.map(book=> <AllBookCard key={book._id} book={book}></AllBookCard>)}
+                {filterBooks.map(book=> <AllBookCard key={book._id} book={book}></AllBookCard>)}
             </div>
         </div>
     );
